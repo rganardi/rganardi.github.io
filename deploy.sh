7fc74f2b2f6e9d775fbe0015340170e7b477dd6e
Ray Ganardi <ray@ganardi.xyz>
2019-05-22 00:51:16 +0700

    add timeout to git daemon during deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh -L9418:localhost:9418 debbie.gcloud 'git daemon --verbose --base-path=$HOME/git/ --reuseaddr'
+ssh -L9418:localhost:9418 debbie.gcloud 'git daemon --verbose --base-path=$HOME/git/ --reuseaddr --timeout=10'
